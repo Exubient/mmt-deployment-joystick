d26dc2f71c2a2cfe0df527b24877f00362cfb91f
alexgue <munguelee@mymusictaste.com>
2018-04-13 20:39:19 +0900

    <feat> frontend : arrow

--- joystick/frontend/src/App.js
+++ joystick/frontend/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import up from './assets/up.png';
+import left from './assets/left.png';
+import right from './assets/right.png';
+import down from './assets/down.png';
+import rightUp from './assets/up-right.png';
+import rightDown from './assets/right-down.png';
+import leftUp from './assets/left-up.png';
+import leftDown from './assets/left-down.png';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      stick: 'joystick up right',
+      answer: [],
+      user: ['joystick up right', 'joystick up left', 'joystick up', 'joystick down'],
+      stick: 'joystick up left', //
       a: false,
       b: false,
       c: true,
@@ -13,13 +23,91 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
+  componentDidMount() {}
+
+  diffCommand() {}
+
+  makeImg(key, index) {
+    if (key === 'joystick up') {
+      return (
+        <div
+          key={index}
+          className="show-icon"
+          style={{ backgroundImage: `url(${up})`, backgroundRepeat: 'no-repeat' }}
+        />
+      );
+    }
+    if (key === 'joystick down') {
+      return (
+        <div
+          key={index}
+          className="show-icon"
+          style={{ backgroundImage: `url(${down})`, backgroundRepeat: 'no-repeat' }}
+        />
+      );
+    }
+    if (key === 'joystick left') {
+      return (
+        <div
+          key={index}
+          className="show-icon"
+          style={{ backgroundImage: `url(${left})`, backgroundRepeat: 'no-repeat' }}
+        />
+      );
+    }
+    if (key === 'joystick right') {
+      return (
+        <div
+          key={index}
+          className="show-icon"
+          style={{ backgroundImage: `url(${right})`, backgroundRepeat: 'no-repeat' }}
+        />
+      );
+    }
+    if (key === 'joystick up right') {
+      return (
+        <div
+          key={index}
+          className="show-icon"
+          style={{ backgroundImage: `url(${rightUp})`, backgroundRepeat: 'no-repeat' }}
+        />
+      );
+    }
+    if (key === 'joystick up left') {
+      return (
+        <div
+          key={index}
+          className="show-icon"
+          style={{ backgroundImage: `url(${leftUp})`, backgroundRepeat: 'no-repeat' }}
+        />
+      );
+    }
+    if (key === 'joystick down right') {
+      return (
+        <div
+          key={index}
+          className="show-icon"
+          style={{ backgroundImage: `url(${rightDown})`, backgroundRepeat: 'no-repeat' }}
+        />
+      );
+    }
+    if (key === 'joystick down left') {
+      return (
+        <div
+          key={index}
+          className="show-icon"
+          style={{ backgroundImage: `url(${leftDown})`, backgroundRepeat: 'no-repeat' }}
+        />
+      );
+    }
+    return null;
   }
 
   render() {
     const {
-      stick, a, b, c, d,
+      user, stick, a, b, c, d,
     } = this.state;
+
     return (
       <div className="container">
         <h1>iStick</h1>
@@ -30,6 +118,7 @@ class App extends Component {
           <div className={d ? 'push-button-d pressed' : 'push-button-d'} />
         </div>
         <p>MMT-iStick</p>
+        <div>{user.map((key, index) => this.makeImg(key, index))}</div>
       </div>
     );
   }
