d8f66a69a17ff1ce1da8562eec518d8f30662f1e
Exubient <henrynicholas09@gmail.com>
2018-04-13 21:39:30 +0900

    admin

--- joystick_2/activities/admin.py
+++ joystick_2/activities/admin.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Note
+# Register your models here.
+
+class _Note(admin.ModelAdmin):
+	list_display=['id', 'body', 'key', 'created_at']
+
+admin.site.register(Note, _Note)
