a9c75512a438aad77dd3f112775dd5ecd6da5364
alexgue <munguelee@mymusictaste.com>
2018-04-13 18:55:11 +0900

    <feat> frontend : add lint setting

--- joystick/frontend/src/App.js
+++ joystick/frontend/src/App.js
@@ -1,33 +1,33 @@
-import React, { Component, Fragment } from "react";
-import "./App.css";
+import React, { Component, Fragment } from 'react';
+import './App.css';
 
 class App extends Component {
   componentDidMount() {
     const KEYS = {};
-    KEYS[32] = "pressed";
-    KEYS[37] = "left";
-    KEYS[38] = "up";
-    KEYS[39] = "right";
-    KEYS[40] = "down";
+    KEYS[32] = 'pressed';
+    KEYS[37] = 'left';
+    KEYS[38] = 'up';
+    KEYS[39] = 'right';
+    KEYS[40] = 'down';
 
-    const joystick = document.getElementById("joystick");
+    const joystick = document.getElementById('joystick');
     const keysDown = {};
 
     function updateJoystickClass() {
-      const classNames = ["joystick"];
-      Object.keys(keysDown).forEach(key => {
+      const classNames = ['joystick'];
+      Object.keys(keysDown).forEach((key) => {
         if (keysDown[key]) classNames.push(key);
       });
-      joystick.className = classNames.join(" ");
+      joystick.className = classNames.join(' ');
     }
 
-    document.addEventListener("keydown", e => {
+    document.addEventListener('keydown', (e) => {
       if (!KEYS[e.which]) return;
       e.preventDefault();
       keysDown[KEYS[e.which]] = true;
       updateJoystickClass();
     });
-    document.addEventListener("keyup", e => {
+    document.addEventListener('keyup', (e) => {
       if (!KEYS[e.which]) return;
       e.preventDefault();
       keysDown[KEYS[e.which]] = false;
