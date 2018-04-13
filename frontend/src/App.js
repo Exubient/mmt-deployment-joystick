a40eb0b995ed77a86ed5b5241305a3d550220c36
alexgue <munguelee@mymusictaste.com>
2018-04-13 16:47:01 +0900

    <feat> Frontend : add joystick feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    function updateJoystickClass() {
+      const classNames = ['joystick'];
+      Object.keys(keysDown).forEach((key) => {
+        if (keysDown[key]) classNames.push(key);
+      });
+      joystick.className = classNames.join(' ');
+    }
+
+    const KEYS = [];
+    KEYS[32] = 'pressed';
+    KEYS[37] = 'left';
+    KEYS[38] = 'up';
+    KEYS[39] = 'right';
+    KEYS[40] = 'down';
+    const joystick = document.getElementById('joystick');
+    const keysDown = {};
+    document.addEventListener('keydown', (e) => {
+      if (!KEYS[e.which]) return;
+      e.preventDefault();
+      keysDown[KEYS[e.which]] = true;
+      updateJoystickClass();
+    });
+    document.addEventListener('keyup', (e) => {
+      if (!KEYS[e.which]) return;
+      e.preventDefault();
+      keysDown[KEYS[e.which]] = false;
+      updateJoystickClass();
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fragment>
+        <h1>iStick</h1>
+        <div className="joystick" id="joystick" />
+        <p>MMT-iStick</p>
+      </Fragment>
     );
   }
 }
